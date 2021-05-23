9d2ab64f4c5cbc5c0b32491dec77d610f06fe5d2
ducnguyen46 <nguyenngocduccl@gmail.com>
2021-05-23 11:24:59 +0700

    url base

--- app/src/main/java/com/ducnguyen46/soc/constant/Constant.java
+++ app/src/main/java/com/ducnguyen46/soc/constant/Constant.java
@@ -1,7 +1,12 @@
 package com.ducnguyen46.soc.constant;
 
 public class Constant {
+    //for gen
     public static final String BASE_URL = "http://10.0.3.2:8080";
+
+    //for android emulator
+//    public static final String BASE_URL = "http://10.0.2.2:8080";
+
 
     public static final String IS_LOGIN = "isLogin";
     public static final String TOKEN_USER = "tokenUser";
